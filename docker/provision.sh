148e0089c8987de121d7121728c71e7a18b34353
Toshiaki Maki <makingx@gmail.com>
2015-05-04 13:06:48 +0900

    use latest docker & install manually

--- docker/provision.sh
+++ docker/provision.sh
@@ -7,37 +7,7 @@ if [ ! -d $VAGRANT_PROVISION ];then
     mkdir -p $VAGRANT_PROVISION
 fi
 
-## JDK8
-# if [ ! -f $VAGRANT_PROVISION/jdk8 ];then
-#     echo "==== Install JDK8 ===="
-#     pushd /tmp > /dev/null
-#     wget -q --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.rpm
-#     rpm -ivh jdk*.rpm*
-#     cat <<'EOF' > /etc/profile.d/java.sh
-# export JAVA_HOME=/usr/java/jdk1.8.0_31
-# export PATH=$PATH:$JAVA_HOME/bin
-# EOF
-#     popd > /dev/null
-#     touch $VAGRANT_PROVISION/jdk8
-# fi
-
-## Maven
-# if [ ! -f $VAGRANT_PROVISION/maven ];then
-#     echo "==== Install Maven ===="
-#     MAVEN_VERSION=3.2.1
-#     mkdir -p /opt/maven
-#     pushd /opt/maven > /dev/null
-#     wget -q http://archive.apache.org/dist/maven/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
-#     tar xzf apache-maven-$MAVEN_VERSION-bin.tar.gz
-#     ln -s /opt/maven/apache-maven-$MAVEN_VERSION /opt/maven/maven3
-#     rm -f apache-maven-$MAVEN_VERSION-bin.tar.gz
-#     cat <<'EOF' > /etc/profile.d/maven.sh
-# export MAVEN_HOME=/opt/maven/maven3
-# export PATH=$PATH:$MAVEN_HOME/bin
-# EOF
-#     popd > /dev/null
-#     touch $VAGRANT_PROVISION/maven
-# fi
+yum update
 
 if [ ! -f $VAGRANT_PROVISION/git ];then
     echo "==== Install Git ===="
@@ -60,9 +30,24 @@ if [ ! -f $VAGRANT_PROVISION/emacs ];then
 fi
 
 if [ ! -f $VAGRANT_PROVISION/docker ];then
+    # Thanks to http://qiita.com/jpshadowapps/items/a68173b4c044cfea2ab6
     echo "==== Install Docker ===="
-    yum -y -q install docker docker-devel
-    chkconfig docker on
-    service docker start
+    pushd /usr/bin
+    wget https://get.docker.io/builds/Linux/x86_64/docker-latest -O docker
+    chmod +x docker
+    pushd /etc/systemd/system
+    wget https://raw.githubusercontent.com/docker/docker/master/contrib/init/systemd/docker.service
+    wget https://raw.githubusercontent.com/docker/docker/master/contrib/init/systemd/docker.socket
+    sed -i -e "s/\-H fd:\/\//-H fd:\/\/ -H tcp:\/\/0.0.0.0:2375/g" docker.service
+    groupadd docker
+    systemctl daemon-reload
+    systemctl start docker
+    systemctl status docker
+    systemctl enable docker.service
+    cat <<'EOF' > /etc/profile.d/docker.sh
+export DOCKER_HOST="tcp://0.0.0.0:2375"
+EOF
+#    firewall-cmd --permanent --zone=public --add-port=2375/tcp
+#    firewall-cmd --reload
     touch $VAGRANT_PROVISION/docker
 fi
